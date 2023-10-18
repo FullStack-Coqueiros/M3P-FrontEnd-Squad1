ef4c43888f0077896e32a3ea3512820c3fccb2a3
William Rocha <uill_pf@hotmail.com>
2023-10-17 22:46:30 -0300

    Estilizando o Layout da Aplicação com StyledComponent

---
+++ LabMedicineAPI/src/layouts/AppLayout.style.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const App = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    background-color: white;
+`
+
+export const SidebarBox = styled.div`
+ height: 90%;
+
+`
+
+export const Main = styled.div`
+ display: flex;
+ flex: 1 0 0;
+ flex-direction: column;
+ width: 90%;
+ height: 100%;
+`
+
+export const Header = styled.div`
+ display: flex;
+ justify-content: center;
+`
+export const Content = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    margin: 0;
+`
