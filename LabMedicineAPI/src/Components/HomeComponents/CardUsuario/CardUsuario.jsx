d352f7f00f365e34de0a52d42ed25b0cf85dc7c0
William Rocha <uill_pf@hotmail.com>
2023-10-25 18:44:29 -0300

    Implementação do CardUsuario

---
+++ LabMedicineAPI/src/Components/HomeComponents/CardUsuario/CardUsuario.jsx
@@ -0,0 +1,27 @@
+import * as Styled from './CardUsuario.style.jsx';
+import { ImUser } from 'react-icons/im';
+
+const CardUsuario = ({ usuario }) => {
+console.log(usuario)
+  return (
+    <>
+      <Styled.CardUsuarioContainer>
+        <Styled.RenderCardUsuario>
+          <Styled.IconCardUsuario>
+            <ImUser/>
+          </Styled.IconCardUsuario>
+
+          <Styled.DadosUsuario>
+            <Styled.DadosNome>{usuario.nomeCompleto}</Styled.DadosNome>
+            <Styled.Dados>{usuario.genero}</Styled.Dados>
+            <Styled.Dados>{usuario.telefone}</Styled.Dados>
+            <Styled.Dados>{usuario.tipo}</Styled.Dados>
+          </Styled.DadosUsuario>
+          
+        </Styled.RenderCardUsuario>
+      </Styled.CardUsuarioContainer>
+    </>
+  );
+};
+
+export default <CardUsuario></CardUsuario>;
