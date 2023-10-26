26e678740921e0c2024b91e09035266a7717bcc6
William Rocha <uill_pf@hotmail.com>
2023-10-25 22:52:18 -0300

    Estilização da CadastroPaciente

---
+++ LabMedicineAPI/src/pages/cadastros/pacientes/CadastroPacientePage.style.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const containerCadastroPaciente = styled.div`
+
+align-items: column;
+width: 100%;
+height: 100%;
+
+div {
+    display: flex;
+    width: 100%;
+}
+`
+export const Header = styled.div`
+display: flex;
+width: 100%;
+
+align-items: column;
+`
+
+export const H3 = styled.h3`
+margin: 30px 30px;
+margin-bottom: 0;
+font-family: "Helvetica", sans-serif;
+font-weight: 580;
+color: grey;
+`
