b36341a9ff235ca234ec9c3bc486b244743cfe64
William Rocha <uill_pf@hotmail.com>
2023-10-26 19:32:42 -0300

    Estilização e implementação do StyledComponent

--- LabMedicineAPI/src/pages/cadastros/consultas/CadastroConsultaPage.jsx
+++ LabMedicineAPI/src/pages/cadastros/consultas/CadastroConsultaPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { HeaderContext } from '../../../context/HeaderContext.jsx';
 import { InputPageConsulta } from '../../../Components/InputComponents/InputPageConsulta.jsx';
+import * as Styled from './CadastroConsultaPage.Style.jsx';
 
 export const CadastroConsultaPage = () => {
     const { setData } = useContext(HeaderContext)
@@ -11,7 +12,12 @@ export const CadastroConsultaPage = () => {
 
     const render = () => {
         return (
-            <></>
+            <Styled.ConsultaContainer>
+            
+                <InputPageConsulta />
+              
+            
+            </Styled.ConsultaContainer>
         )
     }
     return render();
