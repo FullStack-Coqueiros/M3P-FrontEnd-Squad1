450bd834a459593e5ef689eff0b1ec63cd560517
William Rocha <uill_pf@hotmail.com>
2023-10-26 19:22:54 -0300

    Implantação do componente Estilizado

--- LabMedicineAPI/src/Components/InputComponents/InputPageConsulta.jsx
+++ LabMedicineAPI/src/Components/InputComponents/InputPageConsulta.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from 'react-hook-form';
 import { PacienteService } from '../../services/Paciente.service.jsx';
 import { FormConsulta } from '../FormularioComponents/FormConsultas/FormConsulta.jsx';
+import * as Styled from './InputPageConsulta.style.jsx';
 
 export const InputPageConsulta = () => {
     const {
@@ -31,6 +32,20 @@ export const InputPageConsulta = () => {
 
     return (
         <>
+            <Styled.ContainerInput>
+                <h4>Encontre o paciente</h4>
+                <Styled.InputArea onSubmit={handleSubmit(submitInput)}>
+                    <input
+                        placeholder='Digite o nome completo do paciente'
+                        {...register('nome')}
+                    />
+                    <button className='botao botao btn btn-primary' type='submit'><span className='material-symbols-outlined'>Buscar</span></button>
+                </Styled.InputArea>
+            </Styled.ContainerInput> 
+
+            <Styled.ContainerCards>
+                {pacienteSelect && pacienteSelect.map(paciente => <FormConsulta paciente={paciente} key={paciente.id}/>)}
+            </Styled.ContainerCards>
         </>
     )
 }
